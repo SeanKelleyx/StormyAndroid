d68116d3432aefd33da63b623a75473eb0aeb685
Sean Kelley <s.kelley27@gmail.com>
2016-01-21 11:13:20 -0600

    Start mocking the needed objects and methods to test Forecast.java

---
+++ app/src/test/java/com.sean.stormy/weather/ForecastTest.java
@@ -0,0 +1,80 @@
+package com.sean.stormy.weather;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by snkelley on 1/21/2016. :)
+ */
+public class ForecastTest {
+    private Forecast forecast;
+    private JSONObject forecastJson;
+    private JSONObject currently;
+    private JSONObject hourly;
+    private JSONObject hour1;
+    private JSONArray hourlyData;
+    private JSONObject daily;
+    private JSONObject day1;
+    private JSONArray dailyData;
+    final long time = 1453310735;
+    final String summary = "Partly Cloudy";
+    final String icon = "partly-cloudy-day";
+    final double temperature = 52.38;
+    final private String timezone = "America/Los_Angeles";
+
+    @Before
+    public void Setup() throws JSONException {
+        currently = mock(JSONObject.class);
+        hourly = mock(JSONObject.class);
+        hour1 = mock(JSONObject.class);
+        hourlyData = mock(JSONArray.class);
+        daily = mock(JSONObject.class);
+        day1 = mock(JSONObject.class);
+        dailyData = mock(JSONArray.class);
+        forecastJson = mock(JSONObject.class);
+        when(day1.getLong("time")).thenReturn(time);
+        when(day1.getString("icon")).thenReturn(icon);
+        when(day1.getString("summary")).thenReturn(summary);
+        when(day1.getDouble("temperatureMax")).thenReturn(temperature);
+        when(hour1.getLong("time")).thenReturn(time);
+        when(hour1.getString("icon")).thenReturn(icon);
+        when(hour1.getString("summary")).thenReturn(summary);
+        when(hour1.getDouble("temperature")).thenReturn(temperature);
+        when(forecastJson.getString("timezone")).thenReturn(timezone);
+        when(forecastJson.getJSONObject("currently")).thenReturn(currently);
+        when(forecastJson.getJSONObject("hourly")).thenReturn(hourly);
+        when(hourly.getJSONArray("data")).thenReturn(hourlyData);
+        when(hourlyData.length()).thenReturn(1);
+        when(hourlyData.getJSONObject(0)).thenReturn(hour1);
+        when(forecastJson.getJSONObject("daily")).thenReturn(daily);
+        when(daily.getJSONArray("data")).thenReturn(dailyData);
+        when(dailyData.length()).thenReturn(1);
+        when(dailyData.getJSONObject(0)).thenReturn(day1);
+        forecast = new Forecast(forecastJson);
+    }
+
+    @After
+    public void Teardown(){
+        currently = null;
+        hourly = null;
+        hour1 = null;
+        hourlyData = null;
+        daily = null;
+        day1 = null;
+        dailyData = null;
+        forecastJson = null;
+        forecast = null;
+    }
+
+    @Test
+    public void testConstructor() throws JSONException {
+
+    }
+}
