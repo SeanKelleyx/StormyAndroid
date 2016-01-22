46c32e994122310aed385b90119a4bfe69c24635
Sean Kelley <s.kelley27@gmail.com>
2016-01-22 15:26:09 -0600

    Add tests for AlertDialogFragment.java

---
+++ app/src/test/java/com.sean.stormy/ui/AlertDialogFragmentTest.java
@@ -0,0 +1,29 @@
+package com.sean.stormy.ui;
+
+import com.sean.stormy.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by snkelley on 1/22/2016. :)
+ */
+public class AlertDialogFragmentTest {
+    private AlertDialogFragment aDFragment;
+
+    @Before
+    public void Setup(){
+        aDFragment = new AlertDialogFragment();
+    }
+
+    @After
+    public void Teardown(){
+        aDFragment = null;
+    }
+
+    @Test
+    public void testSetters(){
+        aDFragment.setErrorText(R.string.error_with_location);
+    }
+}
